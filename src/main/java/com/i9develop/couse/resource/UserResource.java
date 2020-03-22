57a3b4b1198ccccfc6f2789245cd8107b6fd8d10
Moises Rodrigues <loveloungeoficial@gmail.com>
2020-03-22 01:03:00 -0300

    Service layer, component registration

--- src/main/java/com/i9develop/couse/resource/UserResource.java
+++ src/main/java/com/i9develop/couse/resource/UserResource.java
@@ -1,23 +1,38 @@
 package com.i9develop.couse.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.i9develop.couse.entities.User;
+import com.i9develop.couse.services.UserService;
 
 @RestController
 @RequestMapping(value ="/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
-	@GetMapping
-	public ResponseEntity<User> findAll() {
+	@GetMapping 
+	public ResponseEntity<List<User>> findAll() {
 
-		User u = new User(1L, "	Ana Maria", "ana.maria@gmail.com", "999999999", "123456");
+		List<User> list = service.findAll();
 
-		return ResponseEntity.ok().body(u);
+		return ResponseEntity.ok().body(list);
 
+	}
+	@GetMapping(value="/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		
+		User obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
