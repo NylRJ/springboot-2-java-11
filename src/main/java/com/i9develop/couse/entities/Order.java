85313cd91d83a78e1ce87a3a54d47bd1e317e489
Moises Rodrigues <loveloungeoficial@gmail.com>
2020-03-22 14:10:57 -0300

    OrderStatus enum

--- src/main/java/com/i9develop/couse/entities/Order.java
+++ src/main/java/com/i9develop/couse/entities/Order.java
@@ -12,7 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonFormat.Shape;
+import com.i9develop.couse.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,6 +27,8 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 
+	Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -35,10 +37,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -58,8 +61,23 @@ public class Order implements Serializable {
 		this.moment = moment;
 	}
 
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
+
+		
+
+	}
+
+	public OrderStatus getOrderStatus() {
+
+		return OrderStatus.valueOf(this.orderStatus);
+	}
+
 	public User getClient() {
-		return client;
+		return client;	
 	}
 
 	public void setClient(User client) {
